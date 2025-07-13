ad5c5127aa918f1d85de1b4f65d4cf7af433e931
sairammr <sairam1203mr@gmail.com>
2025-07-13 10:40:36 +0200

    Update Loader.tsx

--- web/app/components/Loader.tsx
+++ web/app/components/Loader.tsx
@@ -21,22 +21,22 @@ export default function SvgLoader({
   };
 
   const LoaderSvg = () => (
-    <div className={cn("flex items-center justify-center", className)}>
+    <div className={cn("flex items-center justify-center bg-transparent", className)}>
       <svg
         viewBox="0 0 164.83 151.5"
         className={cn("block", sizeClasses[size])}
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          className="animate-pulse-custom fill-emerald-500 [animation-delay:-125ms]"
+          className="animate-pulse-custom fill-red-500 [animation-delay:-125ms]"
           d="M117.24,69.24A8,8,0,0,0,115.67,67c-4.88-4-9.8-7.89-14.86-11.62A4.93,4.93,0,0,0,96.93,55c-5.76,1.89-11.4,4.17-17.18,6a4.36,4.36,0,0,0-3.42,4.12c-1,6.89-2.1,13.76-3,20.66a4,4,0,0,0,1,3.07c5.12,4.36,10.39,8.61,15.68,12.76a3.62,3.62,0,0,0,2.92.75c6.29-2.66,12.52-5.47,18.71-8.36a3.49,3.49,0,0,0,1.68-2.19c1.34-7.25,2.54-14.55,3.9-22.58Z"
         />
         <path
-          className="animate-pulse-custom fill-emerald-500 [animation-delay:-250ms]"
+          className="animate-pulse-custom fill-red-500 [animation-delay:-250ms]"
           d="M97.55,38.68A43.76,43.76,0,0,1,98,33.44c.41-2.36-.5-3.57-2.57-4.64C91.1,26.59,87,24,82.66,21.82a6.18,6.18,0,0,0-4-.71C73.45,22.55,68.32,24.25,63.22,26c-3.63,1.21-6.08,3.35-5.76,7.69a26.67,26.67,0,0,1-.6,4.92c-1.08,8.06-1.08,8.08,5.86,11.92,3.95,2.19,7.82,5.75,11.94,6.08s8.76-2.41,13.12-3.93c9.33-3.29,9.33-3.3,9.78-14Z"
         />
         <path
-          className="animate-pulse-custom fill-emerald-500 [animation-delay:-375ms]"
+          className="animate-pulse-custom fill-red-500 [animation-delay:-375ms]"
           d="M66.11,126.56c5.91-.91,11.37-1.7,16.81-2.71a3.3,3.3,0,0,0,1.87-2.17c1-4.06,1.73-8.19,2.84-12.24.54-2-.11-3-1.55-4.15-5-4-9.9-8.12-15-12a6.19,6.19,0,0,0-4.15-1.1c-5.35.66-10.7,1.54-16,2.54A4,4,0,0,0,48.34,97a109.13,109.13,0,0,0-3,12.19,4.47,4.47,0,0,0,1.34,3.6c5.54,4.36,11.23,8.53,16.91,12.69a10.84,10.84,0,0,0,2.57,1.11Z"
         />
         <path
